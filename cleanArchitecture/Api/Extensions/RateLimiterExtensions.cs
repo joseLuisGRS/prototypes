2e53b322aaf47c39dfb546c582bacb3012a283fd
joseLuisGRS <joseluis_gr_27@outlook.com>
2025-03-21 14:38:10 -0600

    Rate Limiting pattern is added

---
+++ cleanArchitecture/Api/Extensions/RateLimiterExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.RateLimiting;
+
+namespace Api.Extensions;
+
+/// <summary>
+/// extension methods for rate limiting.
+/// </summary>
+public static class RateLimiterExtensions
+{
+    const string fixedWindowPolicy = "fixedWindow";
+
+    /// <summary>
+    /// Adds rate limiting functionality to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <param name="configuration">The configuration for rate limiting settings.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddRatelimiting(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        RateLimitSettings rateLimitSettings = configuration.GetSection(RateLimitSettings.SectionName).Get<RateLimitSettings>();
+
+        services.AddRateLimiter(configureOptions =>
+        {
+            configureOptions.AddFixedWindowLimiter(
+                policyName: fixedWindowPolicy, fixedWindow =>
+                {
+                    fixedWindow.PermitLimit = rateLimitSettings.PermitLimit;
+                    fixedWindow.Window = TimeSpan.FromSeconds(Convert.ToDouble(rateLimitSettings.Window));
+                    fixedWindow.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+                    fixedWindow.QueueLimit = int.Parse(rateLimitSettings.QueueLimit);
+                });
+            configureOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+        });
+
+        return services;
+    }
+
+}
---
+++ cleanArchitecture/Application/Commons/RateLimitSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Commons;
+
+public class RateLimitSettings
+{
+    public const string SectionName = "RateLimiting";
+    public int PermitLimit { get; init; }
+    public string Window { get; init; }
+    public string QueueLimit { get; init; }
+}
